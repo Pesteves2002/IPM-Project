c9a4887182167ec8e9c5b3abde87d3147e729160
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2022-04-11 16:47:32 +0100

    Add snapping

--- TargetPractice/sketch.js
+++ TargetPractice/sketch.js
@@ -69,6 +69,8 @@ function setup() {
 }
 
 let dist_targets = 0;
+let cursor_on_rectangle = false;
+let counter = 0;
 
 // Runs every frame and redraws the screen
 function draw() {
@@ -102,6 +104,9 @@ function draw() {
     let x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width);
     let y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height);
 
+    // Draw the user input area
+    drawInputArea();
+
     drawLine(1);
 
     dist_targets = dist(
@@ -111,15 +116,17 @@ function draw() {
       getTargetBounds(2).y
     );
 
+    counter = 0;
+
     // Draw all 18 targets
     for (var i = 0; i < 18; i++) {
       drawTarget(i, x, y);
     }
 
+    if (counter === 18) {
+      cursor_on_rectangle = false;
+    }
     drawLine(0);
-
-    // Draw the user input area
-    drawInputArea();
 
     drawInstructions();
 
@@ -256,7 +263,7 @@ function mousePressed() {
         height
       );
 
-      if (dist(target.x, target.y, virtual_x, virtual_y) < target.w / 2) {
+      if (dist(target.x, target.y, hit_x, hit_y) < target.w / 2) {
         hits++;
         background_colour = HIT_BACKGROUND_COLOUR;
 
@@ -268,9 +275,9 @@ function mousePressed() {
         let previous_target = getTargetBounds(trials[current_trial - 1]);
 
         x1 = previous_target.x;
-        x2 = virtual_x;
+        x2 = hit_x;
         y1 = previous_target.y;
-        y2 = virtual_y;
+        y2 = hit_y;
 
         distance = Math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
         fitts_id = Math.log2(distance / target.w + 1);
@@ -312,21 +319,7 @@ function drawTarget(i, x, y) {
   // Get the location and size for target (i)
   let target = getTargetBounds(i);
 
-  console.log(dist_targets);
-  switch (background_colour) {
-    case DEFAULT_BACKGROUND_COLOR:
-      fill(color(0, 0, 0)); // sets background to black
-      break;
-    case HIT_BACKGROUND_COLOUR:
-      fill(color(0, 15, 5)); // sets background to green
-      break;
-    case MISS_BACKGROUND_COLOUR:
-      fill(color(65, 0, 0)); // sets background to red
-      break;
-    case STREAK_BACKGROUND_COLOR:
-      fill(color(189, 120, 0));
-      break;
-  }
+  noFill();
   // Draw rectangle
   strokeWeight(3);
   stroke(color(255, 255, 255));
@@ -380,6 +373,39 @@ function drawTarget(i, x, y) {
     stroke(0);
     text("2x", target.x, target.y + 10);
     textAlign(LEFT);
+    fill(color(255, 192, 84));
+  }
+
+  let inputTargetX = map(
+    target.x,
+    0,
+    width,
+    inputArea.x,
+    inputArea.x + inputArea.w
+  );
+  let inputTargetY = map(
+    target.y,
+    0,
+    height,
+    inputArea.y,
+    inputArea.y + inputArea.h
+  );
+
+  // Draw rectangle
+  strokeWeight(3);
+  stroke(color(255, 255, 255));
+  rectMode(CENTER);
+
+  rect(
+    inputTargetX,
+    inputTargetY,
+    target.w * (inputArea.w / height),
+    target.w * (inputArea.w / height)
+  );
+  rectMode(CORNER);
+
+  if (!insideRect(i)) {
+    counter++;
   }
 }
 
@@ -614,4 +640,35 @@ function drawInstructions() {
   text("Backgrounds:", inputArea.x, startY);
 
   textFont("Arial", 18); // font size for the majority of the text
+}
+
+let hit_x;
+let hit_y;
+
+function insideRect(i) {
+  let target = getTargetBounds(i);
+  let official_x;
+  let official_y;
+
+  official_x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width);
+  official_y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height);
+
+  if (
+    target.x - dist_targets / 2 < official_x &&
+    official_x < target.x + dist_targets / 2
+  ) {
+    if (
+      target.y - dist_targets / 2 < official_y &&
+      official_y < target.y + dist_targets / 2
+    ) {
+      hit_x = target.x;
+      hit_y = target.y;
+      console.log(official_x, official_y);
+      fill(color(255, 255, 255));
+      circle(target.x, target.y, 0.5 * PPCM);
+      cursor_on_rectangle = true;
+      return true;
+    }
+  }
+  return false;
 }
